ffa264ba69757eca6ae333197b49f3870f42c626
Verdi R-D <verdi@azend.org>
2014-04-21 15:53:01 -0400

    Imported code from mostly empty MVC project

---
+++ EMSPSS/EMSPSS/Models/EmployeeModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace EMSPSS.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class EmployeeDbContext : DbContext
+    {
+        public DbSet<EmployeeDbContext> Employees { get; set; }
+    }
+}
