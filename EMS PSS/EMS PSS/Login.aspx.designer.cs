2bbee61913ff412c0732c8f4812820ea3f9a7a62
Verdi R-D <verdi@azend.org>
2014-04-25 07:31:46 -0400

    Added ability to log out

--- EMS PSS/EMS PSS/Login.aspx.designer.cs
+++ EMS PSS/EMS PSS/Login.aspx.designer.cs
@@ -31,6 +31,15 @@ namespace EMS_PSS {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl loginPage;
         
         /// <summary>
+        /// lblLogout control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblLogout;
+        
+        /// <summary>
         /// lbUserName control.
         /// </summary>
         /// <remarks>
